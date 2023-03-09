2eef58daa50340f367e66ed94ab4c66da606c98e
Phillip Ma <91913161+ma-phillip@users.noreply.github.com>
2023-03-09 00:55:14 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -32,14 +32,14 @@ app.set('view engine', '.hbs');                 // Tell express to use the handl
     !!GENRE Routes
 */
 app.get('/genres', function(req, res)
-    {  
+    {
         let query1 = "SELECT * from Genres";               // Define our query
 
         db.pool.query(query1, function(error, rows, fields){    // Execute the query
 
             res.render('genres', {data: rows});                  // Render the index.hbs file, and also send the renderer
         })                                                      // an object where 'data' is equal to the 'rows' we
-    }); 
+    });
 
 //post for adding genre
 app.post('/add-genre-form', function(req, res){
@@ -51,19 +51,19 @@ app.post('/add-genre-form', function(req, res){
         {
             description = 'NULL'
         }
-    
+
         // Create the query and run it on the database
-        query1 = `INSERT INTO Genres (genre_name, description) VALUES ('${data['input-genre']}', ${description})`;
+        query1 = `INSERT INTO Genres (genre_name, description) VALUES ('${data['input-genre']}', '${data['input-description']}')`;
         db.pool.query(query1, function(error, rows, fields){
-    
+
             // Check to see if there was an error
             if (error) {
-    
+
                 // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
                 console.log(error)
                 res.sendStatus(400);
             }
-    
+
             // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM bsg_people and
             // presents it on the screen
             else
@@ -71,8 +71,8 @@ app.post('/add-genre-form', function(req, res){
                 res.redirect('/');
             }
         })
- }) 
-/*  
+ })
+/*
 
 /*
     !!Companies Routes
@@ -82,14 +82,14 @@ app.post('/add-genre-form', function(req, res){
 //get
 
 app.get('/companies', function(req, res)
-    {  
+    {
         let query1 = "SELECT * from Companies";               // Define our query
 
         db.pool.query(query1, function(error, rows, fields){    // Execute the query
 
             res.render('companies', {data: rows});                  // Render the index.hbs file, and also send the renderer
         })                                                      // an object where 'data' is equal to the 'rows' we
-    }); 
+    });
 
 
 app.post('/add-company-form', function(req, res){
@@ -115,9 +115,9 @@ app.post('/add-company-form', function(req, res){
             res.redirect('/');
         }
     })
-}) 
+})
 
-// post for adding company 
+// post for adding company
 
 // app.js
 
@@ -144,66 +144,91 @@ app.post('/add-company-form', function(req, res){
             res.redirect('/');
         }
     })
-}) 
-/*  
+})
+/*
 
 
 
 /*
-    !!Customers Routes 
+    !!Customers Routes
 */
 app.get('/customers', function(req, res)
-    {  
+    {
         let query1 = "SELECT * from Customers";               // Define our query
 
         db.pool.query(query1, function(error, rows, fields){    // Execute the query
 
             res.render('customers', {data: rows});                  // Render the index.hbs file, and also send the renderer
         })                                                      // an object where 'data' is equal to the 'rows' we
-    });    
-/*  
+    });
+
+    app.post('/add-customer-form', function(req, res){
+        // Capture the incoming data and parse it back to a JS object
+        let data = req.body;
+
+        // Create the query and run it on the database
+        query1 = `INSERT INTO Customers (contact_name, email, phone_number, birthdate, sign_up_date, total_purchases) VALUES ('${data['input-name']}', '${data['input-email']}', '${data['input-number']}', '${data['input-birthday']}', '${data['input-signdate']}', '${data['input-purchases']}')`;
+        db.pool.query(query1, function(error, rows, fields){
+
+            // Check to see if there was an error
+            if (error) {
+
+                // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                console.log(error)
+                res.sendStatus(400);
+            }
+
+            // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM bsg_people and
+            // presents it on the screen
+            else
+            {
+                res.redirect('/');
+            }
+        })
+    })
+/*
 
 /*
-    !!Transactions Routes 
+    !!Transactions Routes
 */
 app.get('/transactions', function(req, res)
-    {  
+    {
         let query1 = "SELECT * from Transactions";               // Define our query
 
         db.pool.query(query1, function(error, rows, fields){    // Execute the query
 
             res.render('transactions', {data: rows});                  // Render the index.hbs file, and also send the renderer
         })                                                      // an object where 'data' is equal to the 'rows' we
-    });    
-/*  
+    });
+/*
 
 /*
-    !!Transaction Details Routes 
+    !!Transaction Details Routes
 */
 app.get('/transaction_details', function(req, res)
-    {  
+    {
         let query1 = "SELECT * from TransactionDetails";               // Define our query
 
         db.pool.query(query1, function(error, rows, fields){    // Execute the query
 
             res.render('transaction_details', {data: rows});                  // Render the index.hbs file, and also send the renderer
         })                                                      // an object where 'data' is equal to the 'rows' we
-    });    
+    });
 /*
 
 /*
-    !!Companies and Games Routes 
+    !!Companies and Games Routes
 */
 app.get('/companies_and_games', function(req, res)
-    {  
+    {
         let query1 = "SELECT * from CompaniesAndGames";               // Define our query
 
         db.pool.query(query1, function(error, rows, fields){    // Execute the query
 
             res.render('companies_and_games', {data: rows});                  // Render the index.hbs file, and also send the renderer
         })                                                      // an object where 'data' is equal to the 'rows' we
-    });   
-    
+    });
+
 // post for adding company and games
 
 app.post('/add-c&g-form', function(req, res){
@@ -229,20 +254,45 @@ app.post('/add-c&g-form', function(req, res){
             res.redirect('/');
         }
     })
-}) 
+})
 
 
 /*
 
-    !!GAMES Routes 
+    !!GAMES Routes
 */
 app.get('/', function(req, res)
+{
+    let query1;
+
+    // If there is no query string, we just perform a basic SELECT
+    if (req.query.gname === undefined)
     {
-        let query1 = 'SELECT * FROM Games';
-        db.pool.query(query1, function(error, rows, fields){
-            res.render('index', {data: rows});
+        query1 = "SELECT * FROM Games;";
+    }
+
+    // If there is a query string, we assume this is a search, and return desired results
+    else
+    {
+        query1 = `SELECT * FROM Games WHERE game_name LIKE "${req.query.gname}%"`
+    }
+
+    let query2 = "SELECT * FROM Genres;";
+
+    db.pool.query(query1, function(error, rows, fields){
+
+        let game = rows;
+
+        // Run the second query
+        db.pool.query(query2, (error, rows, fields) => {
+
+            // Save the planets
+            let genres = rows;
+
+            return res.render('index', {data: game, genres: genres});
         })
-    });
+    })
+});
 
 app.post('/add-game-form', function(req, res){
         // Capture the incoming data and parse it back to a JS object
@@ -269,7 +319,7 @@ app.post('/add-game-form', function(req, res){
             }
         })
 });
- 
+
 
 // Delete function
 app.delete('/delete-game-ajax/', function(req,res,next){
